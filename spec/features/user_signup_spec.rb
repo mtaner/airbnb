7e359a70d7e8f4d0849e4e70cc8024f7b77e823a
srp2930 <s.rimingtonpounder@gmail.com>
2016-06-29 16:08:40 +0100

    Refactoring 1

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -31,12 +31,3 @@ feature 'Sign Up' do
     expect(page).to have_content('Email must not be blank')
   end
 end
-
-
-def signup(email: 'diamond.oliver2@gmail.com', password: 'waffles', password_confirmation: 'waffles')
-  visit('/signup');
-  fill_in('email', with: email)
-  fill_in('password', with: password)
-  fill_in('password_confirmation', with: password_confirmation)
-  click_button('Submit')
-end
