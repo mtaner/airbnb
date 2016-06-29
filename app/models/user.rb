4a4d32e32c178ed86d123333bd3dae453f34ec0e
mtaner <mervetaner88@gmail.com>
2016-06-29 10:29:31 +0100

    Code review refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,14 +22,7 @@ class User
 
   def self.authenticate(email, password)
     user = first(email: email)
-
-    if user && BCrypt::Password.new(user.password_digest) == password
-      user
-    else
-      nil
-    end
+    user if user && BCrypt::Password.new(user.password_digest) == password
   end
-
-
 
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,5 +1,14 @@
 def add_space
+	signup
 	visit '/myspaces/new'
 	fill_in 'property', with: 'One bedroom flat in Holborn'
 	click_button("Add space")
+end
+
+def signup(email: 'diamond.oliver2@gmail.com', password: 'waffles', password_confirmation: 'waffles')
+  visit('/signup');
+  fill_in('email', with: email)
+  fill_in('password', with: password)
+  fill_in('password_confirmation', with: password_confirmation)
+  click_button('Submit')
 end
